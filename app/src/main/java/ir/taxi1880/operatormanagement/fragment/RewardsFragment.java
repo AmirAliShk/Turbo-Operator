32b9763ae37e8e0003a386eefa79b279ee81f161
elahe <elahenajafyi@gmail.com>
2020-06-23 13:39:04 +0430

    add TypefaceUtil to RewardsFragment

--- app/src/main/java/ir/taxi1880/operatormanagement/fragment/RewardsFragment.java
+++ app/src/main/java/ir/taxi1880/operatormanagement/fragment/RewardsFragment.java
@@ -23,6 +23,7 @@ import ir.taxi1880.operatormanagement.R;
 import ir.taxi1880.operatormanagement.adapter.RewardAdapter;
 import ir.taxi1880.operatormanagement.app.EndPoints;
 import ir.taxi1880.operatormanagement.app.MyApplication;
+import ir.taxi1880.operatormanagement.helper.TypefaceUtil;
 import ir.taxi1880.operatormanagement.model.RewardsModel;
 import ir.taxi1880.operatormanagement.okHttp.RequestHelper;
 import ir.taxi1880.operatormanagement.push.AvaCrashReporter;
@@ -47,6 +48,7 @@ public class RewardsFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_rewards, container, false);
+    TypefaceUtil.overrideFonts(view);
     unbinder = ButterKnife.bind(this, view);
     getRewards();
     return view;
