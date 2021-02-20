91b6b0519f241014ec652ce44bb7e4d24eacda30
maryam shokohi <s.m.s.shokouhi14@gmail.com>
2021-02-20 12:23:29 +0330

    add pending complaint xml

--- app/src/main/java/ir/taxi1880/operatormanagement/fragment/PendingComplaintFragment.java
+++ app/src/main/java/ir/taxi1880/operatormanagement/fragment/PendingComplaintFragment.java
@@ -9,14 +9,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 import ir.taxi1880.operatormanagement.R;
 
 public class PendingComplaintFragment extends Fragment {
+    Unbinder unbinder;
+
+    @OnClick(R.id.btnSaveResult)
+    void onSaveResult(){
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
        View view = inflater.inflate(R.layout.fragment_pending_complaint, container, false);
-
+        unbinder = ButterKnife.bind(this, view);
 
         return view;
     }
