e5d7b0c80a8b949f1bf851e8a65e063a4b9f2aa9
shuku <s.m.s.shokouhi14@gmail.com>
2021-08-30 10:00:01 +0430

    update check list

--- app/src/main/java/ir/taxi1880/operatormanagement/receiver/PushReceiver.java
+++ app/src/main/java/ir/taxi1880/operatormanagement/receiver/PushReceiver.java
@@ -17,6 +17,7 @@ import org.json.JSONObject;
 
 import androidx.core.app.NotificationCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
 import ir.taxi1880.operatormanagement.R;
 import ir.taxi1880.operatormanagement.activity.SplashActivity;
 import ir.taxi1880.operatormanagement.app.Constant;
@@ -31,155 +32,154 @@ import static ir.taxi1880.operatormanagement.app.Keys.KEY_REFRESH_USER_STATUS;
 import static ir.taxi1880.operatormanagement.app.Keys.KEY_USER_STATUS;
 
 public class PushReceiver extends BroadcastReceiver {
-  public static final String TAG = PushReceiver.class.getSimpleName();
+    public static final String TAG = PushReceiver.class.getSimpleName();
 
-  @Override
-  public void onReceive(Context context, Intent intent) {
-    try {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            Bundle bundle = intent.getExtras();
+            String result = bundle.getString(Keys.KEY_MESSAGE);
+            int type = bundle.getInt(Keys.KEY_BROADCAST_TYPE);
 
-      Bundle bundle = intent.getExtras();
-      String result = bundle.getString(Keys.KEY_MESSAGE);
-      int type = bundle.getInt(Keys.KEY_BROADCAST_TYPE);
+            JSONObject object = new JSONObject(result);
+            String strMessage = object.getString("message");
+            Log.i(TAG, "AMIRREZA=> onReceive: " + strMessage);
 
-      JSONObject object = new JSONObject(result);
-      String strMessage = object.getString("message");
-      Log.i(TAG, "AMIRREZA=> onReceive: " + strMessage);
+            JSONObject messages = new JSONObject(strMessage);
+            String typee = messages.getString("type");
 
-      JSONObject messages = new JSONObject(strMessage);
-      String typee = messages.getString("type");
+            if (typee.equals("callerInfo")) {
+                int exten = messages.getInt("exten");
+                String participant = messages.getString("participant");
+                String queue = messages.getString("queue");
+                String voipId = messages.getString("voipId");
 
-      if (typee.equals("callerInfo")) {
-        int exten = messages.getInt("exten");
-        String participant = messages.getString("participant");
-        String queue = messages.getString("queue");
-        String voipId = messages.getString("voipId");
+                MyApplication.prefManager.setVoipId(voipId);
+                MyApplication.prefManager.setQueue(queue);
 
-        MyApplication.prefManager.setVoipId(voipId);
-        MyApplication.prefManager.setQueue(queue);
+            } else if (typee.equals("userStatus")) {
+                boolean status = messages.getBoolean("status");
+                String message = messages.getString("message");
 
-      } else if (typee.equals("userStatus")) {
-        boolean status = messages.getBoolean("status");
-        String message = messages.getString("message");
+                if (status)
+                    MyApplication.prefManager.setActivateStatus(true);
+                else
+                    MyApplication.prefManager.setActivateStatus(false);
 
-        if (status)
-          MyApplication.prefManager.setActivateStatus(true);
-        else
-          MyApplication.prefManager.setActivateStatus(false);
+                if (MyApplication.prefManager.isAppRun()) {
+                    new GeneralDialog()
+                            .title("هشدار")
+                            .message(message)
+                            .cancelable(false)
+                            .firstButton("باشه", null)
+                            .isSingleMode(true)
+                            .show();
+                } else {
+                    createUserStatusNotification(MyApplication.context, message);
+                }
 
-        if (MyApplication.prefManager.isAppRun()) {
-          new GeneralDialog()
-                  .title("هشدار")
-                  .message(message)
-                  .cancelable(false)
-                  .firstButton("باشه", null)
-                  .isSingleMode(true)
-                  .show();
-        } else {
-          createUserStatusNotification(MyApplication.context, message);
+                LocalBroadcastManager broadcaster = LocalBroadcastManager.getInstance(MyApplication.context);
+                Intent broadcastIntent = new Intent(KEY_REFRESH_USER_STATUS);
+                broadcastIntent.putExtra(KEY_USER_STATUS, status);
+                broadcastIntent.putExtra(KEY_MESSAGE_USER_STATUS, message);
+                broadcaster.sendBroadcast(broadcastIntent);
+
+            } else if (typee.equals("message")) {
+                String value = messages.getString("value");
+                String messageType = messages.getString("messageType");
+
+                String title = "پیام";
+                if (messageType.equals("message"))
+                    title = "پیام";
+                else if (messageType.equals("announcement"))
+                    title = "اطلاعیه";
+
+                if (MyApplication.prefManager.isAppRun()) {
+                    new GeneralDialog()
+                            .title(title)
+                            .message(value)
+                            .cancelable(false)
+                            .firstButton("باشه", null)
+                            .isSingleMode(true)
+                            .show();
+                } else {
+                    createMessageNotification(MyApplication.context, messageType, value);
+                }
+
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
-        LocalBroadcastManager broadcaster = LocalBroadcastManager.getInstance(MyApplication.context);
-        Intent broadcastIntent = new Intent(KEY_REFRESH_USER_STATUS);
-        broadcastIntent.putExtra(KEY_USER_STATUS, status);
-        broadcastIntent.putExtra(KEY_MESSAGE_USER_STATUS, message);
-        broadcaster.sendBroadcast(broadcastIntent);
-
-      } else if (typee.equals("message")) {
-        String value = messages.getString("value");
-        String messageType = messages.getString("messageType");
-
-        String title = "پیام";
-        if (messageType.equals("message"))
-          title = "پیام";
-        else if (messageType.equals("announcement"))
-          title = "اطلاعیه";
-
-        if (MyApplication.prefManager.isAppRun()) {
-          new GeneralDialog()
-                  .title(title)
-                  .message(value)
-                  .cancelable(false)
-                  .firstButton("باشه", null)
-                  .isSingleMode(true)
-                  .show();
-        } else {
-          createMessageNotification(MyApplication.context, messageType, value);
-        }
-
-      }
-
-    } catch (JSONException e) {
-      e.printStackTrace();
-    }
-  }
-
-  public void createMessageNotification(Context context, String type, String value) {
-
-    NotificationManager mNotificationManager;
-    String CHANNEL = "pushChannel";
-    Intent intent = new Intent(context, SplashActivity.class);
-    RemoteViews collapsedView = new RemoteViews(context.getPackageName(), R.layout.notification_collapsed);
-
-    if (type.equals(Constant.PUSH_NOTIFICATION_MESSAGE_TYPE)) {
-      DataHolder.getInstance().setPushType(Constant.PUSH_NOTIFICATION_MESSAGE_TYPE);
-    } else if (type.equals(Constant.PUSH_NOTIFICATION_ANNOUNCEMENT_TYPE)) {
-      DataHolder.getInstance().setPushType(Constant.PUSH_NOTIFICATION_ANNOUNCEMENT_TYPE);
     }
 
-    collapsedView.setTextViewText(R.id.txtValue, value);
-    PendingIntent pendingIntent = PendingIntent.getActivity(context, Constant.PUSH_NOTIFICATION_REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    public void createMessageNotification(Context context, String type, String value) {
 
-    collapsedView.setOnClickPendingIntent(R.id.linearNotif, pendingIntent);
+        NotificationManager mNotificationManager;
+        String CHANNEL = "pushChannel";
+        Intent intent = new Intent(context, SplashActivity.class);
+        RemoteViews collapsedView = new RemoteViews(context.getPackageName(), R.layout.notification_collapsed);
+
+        if (type.equals(Constant.PUSH_NOTIFICATION_MESSAGE_TYPE)) {
+            DataHolder.getInstance().setPushType(Constant.PUSH_NOTIFICATION_MESSAGE_TYPE);
+        } else if (type.equals(Constant.PUSH_NOTIFICATION_ANNOUNCEMENT_TYPE)) {
+            DataHolder.getInstance().setPushType(Constant.PUSH_NOTIFICATION_ANNOUNCEMENT_TYPE);
+        }
+
+        collapsedView.setTextViewText(R.id.txtValue, value);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, Constant.PUSH_NOTIFICATION_REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        collapsedView.setOnClickPendingIntent(R.id.linearNotif, pendingIntent);
 //    {"type":"userStatus","status":false,"message":"افتادی بیرون :))"}
-    NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context.getApplicationContext(), "push")
-            .setSmallIcon(R.drawable.ic_baseline_message_24)
-            .setContent(collapsedView)
-            .setStyle(new NotificationCompat.BigTextStyle().bigText(value))
-            .setContentText(value)
-            .setOngoing(false)
-            .setVibrate(new long[]{200, 200})
-            .setAutoCancel(false)
-            .setSound(Uri.parse(MyApplication.SOUND + R.raw.short_notification))
-            .setStyle(new NotificationCompat.DecoratedCustomViewStyle());
-    mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context.getApplicationContext(), "push")
+                .setSmallIcon(R.drawable.ic_baseline_message_24)
+                .setContent(collapsedView)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(value))
+                .setContentText(value)
+                .setOngoing(false)
+                .setVibrate(new long[]{200, 200})
+                .setAutoCancel(false)
+                .setSound(Uri.parse(MyApplication.SOUND + R.raw.short_notification))
+                .setStyle(new NotificationCompat.DecoratedCustomViewStyle());
+        mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      NotificationChannel channel = new NotificationChannel(CHANNEL, "turboTaxi", NotificationManager.IMPORTANCE_HIGH);
-      mNotificationManager.createNotificationChannel(channel);
-      mBuilder.setChannelId(CHANNEL);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL, "turboTaxi", NotificationManager.IMPORTANCE_HIGH);
+            mNotificationManager.createNotificationChannel(channel);
+            mBuilder.setChannelId(CHANNEL);
+        }
+
+        mNotificationManager.notify(Constant.PUSH_NOTIFICATION_ID, mBuilder.build());
+
     }
 
-    mNotificationManager.notify(Constant.PUSH_NOTIFICATION_ID, mBuilder.build());
+    public void createUserStatusNotification(Context context, String value) {
 
-  }
+        NotificationManager mNotificationManager;
+        String CHANNEL = "pushStatusChannel";
+        Intent intent = new Intent(context, SplashActivity.class);
 
-  public void createUserStatusNotification(Context context, String value) {
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, Constant.USER_STATUS_NOTIFICATION_REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-    NotificationManager mNotificationManager;
-    String CHANNEL = "pushStatusChannel";
-    Intent intent = new Intent(context, SplashActivity.class);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context.getApplicationContext(), "statusPush")
+                .setSmallIcon(R.drawable.ic_baseline_remove_from_queue_24)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(value))
+                .setContentText(value)
+                .setOngoing(false)
+                .setVibrate(new long[]{200, 200})
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setSound(Uri.parse(MyApplication.SOUND + R.raw.short_notification));
+        mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-    PendingIntent pendingIntent = PendingIntent.getActivity(context, Constant.USER_STATUS_NOTIFICATION_REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL, "turboTaxi_userStatus", NotificationManager.IMPORTANCE_HIGH);
+            mNotificationManager.createNotificationChannel(channel);
+            mBuilder.setChannelId(CHANNEL);
+        }
 
-    NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context.getApplicationContext(), "statusPush")
-            .setSmallIcon(R.drawable.ic_baseline_remove_from_queue_24)
-            .setStyle(new NotificationCompat.BigTextStyle().bigText(value))
-            .setContentText(value)
-            .setOngoing(false)
-            .setVibrate(new long[]{200, 200})
-            .setAutoCancel(true)
-            .setContentIntent(pendingIntent)
-            .setSound(Uri.parse(MyApplication.SOUND + R.raw.short_notification));
-    mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(Constant.USER_STATUS_NOTIFICATION_ID, mBuilder.build());
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      NotificationChannel channel = new NotificationChannel(CHANNEL, "turboTaxi_userStatus", NotificationManager.IMPORTANCE_HIGH);
-      mNotificationManager.createNotificationChannel(channel);
-      mBuilder.setChannelId(CHANNEL);
     }
-
-    mNotificationManager.notify(Constant.USER_STATUS_NOTIFICATION_ID, mBuilder.build());
-
-  }
 
 }
