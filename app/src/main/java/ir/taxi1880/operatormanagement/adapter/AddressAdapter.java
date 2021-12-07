dda33532081a5a76bcceebab56b7c76718c1ad69
fatemeh <fatimanoori1998@gmail.com>
2021-12-07 17:13:04 +0330

    change AddressAdapter.java

--- app/src/main/java/ir/taxi1880/operatormanagement/adapter/AddressAdapter.java
+++ app/src/main/java/ir/taxi1880/operatormanagement/adapter/AddressAdapter.java
@@ -1,11 +1,13 @@
 package ir.taxi1880.operatormanagement.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -13,81 +15,99 @@ import java.util.List;
 
 import ir.taxi1880.operatormanagement.R;
 import ir.taxi1880.operatormanagement.model.AddressArr;
+import ir.taxi1880.operatormanagement.model.OperatorModel;
+import ir.taxi1880.operatormanagement.push.AvaCrashReporter;
 
 public class AddressAdapter extends ArrayAdapter<AddressArr> {
     Context context;
     int resource, textViewResourceId;
-    List<AddressArr> items, tempItems, suggestions;
+    List<AddressArr> addressModels, addressFilterModels;
 
     public AddressAdapter(Context context, int resource, int textViewResourceId, List<AddressArr> items) {
         super(context, resource, textViewResourceId, items);
         this.context = context;
         this.resource = resource;
         this.textViewResourceId = textViewResourceId;
-        this.items = items;
-        tempItems = new ArrayList<AddressArr>(items); // this makes the difference.
-        suggestions = new ArrayList<AddressArr>();
+        this.addressModels = items;
+        addressFilterModels = items;
+        getFilter();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = convertView;
-        if (convertView == null) {
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            view = inflater.inflate(R.layout.item_custom_auto_complete, parent, false);
-        }
-        AddressArr addressArr = items.get(position);
-        if (addressArr != null) {
-            TextView lblName = (TextView) view.findViewById(R.id.lbl_address);
-            if (lblName != null)
-                lblName.setText(addressArr.address);
+        try {
+            AddressArr addressArr = addressModels.get(position);
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.item_custom_auto_complete, parent, false);
+            }
+            if (addressArr != null) {
+                TextView lblName = (TextView) view.findViewById(R.id.lbl_address);
+                if (lblName != null)
+                    lblName.setText(addressArr.address);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            AvaCrashReporter.send(e, "AddressAdapter class, getView method");
         }
         return view;
     }
 
     @Override
+    public int getCount() {
+        return addressModels.size();
+    }
+
+    private NameFilter nameFilter;
+
+
+    @Override
     public Filter getFilter() {
+        if (nameFilter == null) {
+            nameFilter = new NameFilter();
+        }
         return nameFilter;
+
     }
 
     /**
      * Custom Filter implementation for custom suggestions we provide.
      */
-    Filter nameFilter = new Filter() {
-        @Override
-        public CharSequence convertResultToString(Object resultValue) {
-            String str = ((AddressArr) resultValue).address;
-            return str;
-        }
-
+    private class NameFilter extends Filter {
         @Override
         protected FilterResults performFiltering(CharSequence constraint) {
-            if (constraint != null) {
-                suggestions.clear();
-                for (AddressArr addressArr : tempItems) {
-                    if (addressArr.address.toLowerCase().contains(constraint.toString().toLowerCase())) {
-                        suggestions.add(addressArr);
+            FilterResults results = new FilterResults();
+            if (constraint != null && constraint.length() > 0) {
+
+                ArrayList<AddressArr> filterList = new ArrayList<>();
+
+                for (int i = 0; i < addressFilterModels.size(); i++) {
+                    if (addressFilterModels.get(i).address.contentEquals(constraint)) {
+                        Log.i("TAG", "performFiltering: "+constraint.toString());
+                        Log.i("TAG", "performFiltering: "+addressFilterModels.get(i).address);
+                        AddressArr addressArr = new AddressArr();
+                        addressArr.address = addressFilterModels.get(i).address;
+                        filterList.add(addressArr);
                     }
                 }
-                FilterResults filterResults = new FilterResults();
-                filterResults.values = suggestions;
-                filterResults.count = suggestions.size();
-                return filterResults;
+                results.count = filterList.size();
+                results.values = filterList;
             } else {
-                return new FilterResults();
+                results.count = addressFilterModels.size();
+                results.values = addressFilterModels;
             }
+            return results;
+
         }
 
         @Override
         protected void publishResults(CharSequence constraint, FilterResults results) {
-            List<AddressArr> filterList = (ArrayList<AddressArr>) results.values;
-            if (results != null && results.count > 0) {
-                clear();
-                for (AddressArr addressArr : filterList) {
-                    add(addressArr);
-                    notifyDataSetChanged();
-                }
-            }
+            addressModels = (ArrayList<AddressArr>) results.values;
+            notifyDataSetChanged();
+
         }
-    };
+    }
+
+    ;
 }
